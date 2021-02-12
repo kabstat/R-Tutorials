753a326193cfd06c70a09bf8ae96253c3cff328d
Uchechukwu Ikeaba <43301518+kabstat@users.noreply.github.com>
2021-02-11 23:11:07 -0500

    Update 001 Data Preprocessing.R

--- 001 Data Preprocessing.R
+++ 001 Data Preprocessing.R
@@ -1,3 +1,40 @@
+# Data Preprocessing
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+
+
+# Data Preprocessing Template
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+
+                                             
 # 1.0 Load libraries ----
 
 # Work horse packages
@@ -182,22 +219,16 @@ sales_by_year_cat_2_tbl %>%
 
 
 # 7.0 Writing Files ----
-
 fs::dir_create("00_data/bike_sales/data_wrangled_student")
 
 # 7.1 Excel ----
-
 bike_orderlines_wrangled_tbl %>%
     write_xlsx("00_data/bike_sales/data_wrangled_student/bike_orderlines.xlsx")
 
-
 # 7.2 CSV ----
-
 bike_orderlines_wrangled_tbl %>%
     write_csv("00_data/bike_sales/data_wrangled_student/bike_orderlines.csv")
 
-
 # 7.3 RDS ----
-
 bike_orderlines_wrangled_tbl %>%
     write_rds("00_data/bike_sales/data_wrangled_student/bike_orderlines.rds")
